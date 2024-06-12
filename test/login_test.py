db0a1247b9c55cf6bb8d66bc32ba683c1601b7c5
Andrei Shabaev <shabaevandrew@gmail.com>
2024-06-12 18:37:19 +0300

    add test via allureTestOps

--- test/login_test.py
+++ test/login_test.py
@@ -1,2 +1,23 @@
+import allure
+
+
+@allure.id("32848")
+@allure.title("Successful login")
+@allure.label("suite", "manual test")
+@allure.label("owner", "allure8")
 def test_login_successful():
-    pass
+    with allure.step("Открыть страницу https://testing.github.com"):
+        pass
+    with allure.step("Перейти на вкладку auth"):
+        pass
+    with allure.step("Ввести валидные данные пользователя"):
+        with allure.step("Логин: test"):
+            pass
+        with allure.step("Пароль: test"):
+            pass
+    with allure.step("Нажать кнопку submit"):
+        pass
+    with allure.step("Отобразилась страница с профилем пользователя"):
+        pass
+    with allure.step("Проверить в логах информацию об успешной регистрации"):
+        pass
